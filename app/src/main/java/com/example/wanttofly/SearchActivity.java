6211d67d9c3d0dc22d4fe57ebc574da5945042cf
Ethan Lee <33e.lee@gmail.com>
2022-09-18 19:02:34 -0700

    Add basic static splash screen

---
+++ app/src/main/java/com/example/wanttofly/SearchActivity.java
@@ -0,0 +1,23 @@
+package com.example.wanttofly;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    public static Intent getIntent(Context context) {
+        return new Intent(context, SearchActivity.class);
+    }
+}
