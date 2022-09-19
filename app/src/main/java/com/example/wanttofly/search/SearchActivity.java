370564b150a054120c69a109b7e82b5b4a8a3b99
Ethan Lee <33e.lee@gmail.com>
2022-09-18 19:21:01 -0700

    Add dark theme splash screen video

--- app/src/main/java/com/example/wanttofly/search/SearchActivity.java
+++ app/src/main/java/com/example/wanttofly/search/SearchActivity.java
@@ -1,4 +1,4 @@
-package com.example.wanttofly;
+package com.example.wanttofly.search;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
---
+++ app/src/main/java/com/example/wanttofly/splash/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.wanttofly.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import com.example.wanttofly.R;
+import com.example.wanttofly.search.SearchActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playSplashScreenVideo();
+    }
+
+    private void playSplashScreenVideo() {
+        VideoView videoView = findViewById(R.id.videoView);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" +
+                R.raw.want_to_fly_dark));
+        videoView.setOnCompletionListener(mediaPlayer -> {
+                    startActivity(SearchActivity.getIntent(this));
+                    finish();
+                }
+        );
+        videoView.start();
+    }
+}
