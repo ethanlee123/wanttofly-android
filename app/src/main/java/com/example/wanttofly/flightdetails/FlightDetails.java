3cc6d5bc22d80b753b2f29acc55b589044b83edf
Ethan Lee <33e.lee@gmail.com>
2022-10-05 21:25:12 -0700

    Add basic xml for search activity

---
+++ app/src/main/java/com/example/wanttofly/flightdetails/FlightDetails.java
@@ -0,0 +1,17 @@
+package com.example.wanttofly.flightdetails;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.wanttofly.R;
+
+public class FlightDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_details);
+    }
+}
