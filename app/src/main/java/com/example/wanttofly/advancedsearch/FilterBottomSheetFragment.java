af5b6cc42a2d55587ff6e10546b6d55eb59b633f
Ethan Lee <33e.lee@gmail.com>
2022-11-20 21:41:08 -0800

    Add sorter fragment (no logic, just UI)

--- app/src/main/java/com/example/wanttofly/advancedsearch/FilterBottomSheetFragment.java
+++ app/src/main/java/com/example/wanttofly/advancedsearch/FilterBottomSheetFragment.java
@@ -1,5 +1,6 @@
 package com.example.wanttofly.advancedsearch;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,15 +8,41 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.wanttofly.R;
+import com.example.wanttofly.search.SearchViewModel;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class FilterBottomSheetFragment extends BottomSheetDialogFragment {
     public static final String TAG = "FilterBottomSheetFragment";
+
+    BottomSheetDialog dialog;
+    SearchViewModel viewModel;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        dialog = (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
+        return dialog;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_advanced_search, container, false);
+        View v = inflater.inflate(R.layout.fragment_advanced_search, container, false);
+        viewModel = new ViewModelProvider(requireActivity()).get(SearchViewModel.class);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public int getTheme() {
+        return R.style.AppBottomSheetDialogTheme;
     }
 }
